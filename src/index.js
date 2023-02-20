96ac87606ad8acf7b96c0adf7a6324c72543a477
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-02-20 20:50:55 +0100

    fix: change root to not create conflicts when routing to another file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ var liveServer = require("live-server");
 
 var params = {
     port: 4200,
-    host: "0.0.0.0",
-    // get the root of the project
-    root: __dirname
+    host: "0.0.0.0"
 };
 
 liveServer.start(params);
