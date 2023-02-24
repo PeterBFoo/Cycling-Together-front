3efa9953539f543e48d75868a28d9623b8b18029
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-02-24 19:06:42 +0100

    refactor: filters code

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -4,5 +4,23 @@ var utils = {
         var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
         var results = regex.exec(location.search);
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    },
+
+    getUrlSearchParams: function () {
+        let params = window.location.search
+        return new URLSearchParams(params);
+    },
+
+    getParamNames: function (names) {
+        let paramNames = [];
+        let searchParams = this.getUrlSearchParams();
+
+        names.forEach(name => {
+            if (searchParams.get(name)) {
+                paramNames.push(name);
+            }
+        });
+
+        return paramNames;
     }
 }
